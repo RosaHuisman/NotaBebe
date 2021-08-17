d668b4f7d6594a329aae2f6a5350d2f1796445e6
webtoufik <webtoufik@gmail.com>
2021-08-17 14:38:05 +0200

    features-contact-footer (sans le logo React)

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -3,14 +3,11 @@ import React from 'react';
 import ContactFooter from '../ContactFooter';
 
 // == Import
-import reactLogo from './react-logo.svg';
 import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
     <ContactFooter />
   </div>
 );
