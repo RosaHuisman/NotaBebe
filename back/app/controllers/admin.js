3ec3d52f95f57713ec3ecc6ab18e4505b5876d3a
blandinejar <blandine.jarysta@gmail.com>
2021-08-18 23:37:26 +0200

    WIP admin routes

--- back/app/controllers/admin.js
+++ back/app/controllers/admin.js
@@ -1,105 +1,171 @@
 const bcrypt = require('bcrypt');
 
 const adminDataMapper = require('../dataMappers/admin');
-
+const userDataMapper = require('../dataMappers/user');
 
 const adminController = {
 
-    /**
-     * Access signup form
-     * 
-     * @param {Request} request 
-     * @param {Response} response 
-     */
-    register: (_, response) => {
-        response.json({ data: user });
-    },
+    // ADMIN USER
 
-    /**
-     * Recording of data received by the login form
-     * 
-     * @param {Request} request 
-     * @param {Response} response 
-     */
-    registerSave: async (request, response) => {
-
+    addUser: async (request, response, next) => {
         try {
-            console.log(request.body);
-            // Vérifier la cohérences des données
-            const firstname = request.body.firstname;
-            const lastname = request.body.lastname;
-            const email = request.body.email;
-            const password = request.body.password;
-            const passwordConfirm = request.body.passwordConfirm;
 
-            const errors = [];
-            console.log(firstname.length);
+            const newUser = await adminDataMapper.insertOne(request.body);
 
-            // On regarde la taille de la chaine de caractère
-            if (firstname.length === 0) {
-                // Si elle est à 0, on rajoute une error à notre tableau
-                errors.push("Le prénom est obligatoire");
+            if (!newUser) {
+                return next();
             }
 
-            if (lastname.length === 0) {
-                errors.push("Le nom de famille est obligatoire");
-            }
-
-            if (password.length === 0) {
-                errors.push("Le mot de passe est obligatoire");
-            }
-
-            // email valide ?
-            if (!email.includes('@')) {
-                errors.push("L'email n'est pas valide");
-            }
-
-            // mots de passes identiques ?
-            if (password !== passwordConfirm) {
-                errors.push("Les mots de passe sont différents");
-            }
-
-            // On compte le nombre d'user avec l'email fourni
-            const emailDejaPresent = await adminDataMapper.countEmail(email);
-
-
-            // Si il est > 0
-            if (emailDejaPresent > 0) {
-                // On rajoute une erreur
-                errors.push("Un compte existe déjà avec cet email");
-            }
-
-            if (errors.length > 0) {
-                // En cas d'erreurs détectées, on fait un rendu de la vue register
-                // En lui transmettant notre tableau d'erreur.
-                response.render('signup', {
-                    error: errors
-                });
-                return;
-            }
-
-            const hash = bcrypt.hashSync(password, 10);
-
-            // Enregistrer ces données en BDD
-            const user = await adminDataMapper.insertOne({
-                firstname,
-                lastname,
-                email,
-                password: hash
-            });
-
-            // Connecter l'utilisateur (l'enregistrer en session)
-            request.session.user = user;
-
-            // Rediriger l'internaute sur sa page profil
-            response.redirect('/');
+            response.json({ data: newUser });
 
         } catch (error) {
-            console.log(error);
+            console.error(error);
             response.json({ error: error.message });
         }
+    },
 
-    }
+    modifyUser: async (request, response) => {
+
+    },
+
+    deleteUser: async (request, response, next) => {
+        try {
+
+            const id = Number(request.params.id);
+
+            if (isNaN(id)) {
+                return next();
+            }
+
+            await adminDataMapper.delete(id);
+
+            response.json('User bien supprimé');
+
+
+        } catch (error) {
+            console.error(error);
+            response.json({ error: error.message });
+        }
+    },
+
+    // ADMIN CHILD
+
+    addChild: async (request, response, next) => {
+        try {
+
+            //! ne fonctionne pas encore, il faudrait pouvoir inclure le request.params.id pour récupérer l'id du parent
+            const newChild = await adminDataMapper.addChild(request.body);
+
+            if (!newChild) {
+                return next();
+            }
+
+            response.json({ data: newChild });
+
+        } catch (error) {
+            console.error(error);
+            response.json({ error: error.message });
+        }
+    },
+
+
+    // /**
+    //  * Access signup form
+    //  * 
+    //  * @param {Request} request 
+    //  * @param {Response} response 
+    //  */
+    // register: (_, response) => {
+    //     response.json({ data: user });
+    // },
+
+    // /**
+    //  * Recording of data received by the login form
+    //  * 
+    //  * @param {Request} request 
+    //  * @param {Response} response 
+    //  */
+    // registerSave: async (request, response) => {
+
+    //     try {
+    //         console.log(request.body);
+    //         // Vérifier la cohérences des données
+    //         const firstname = request.body.firstname;
+    //         const lastname = request.body.lastname;
+    //         const email = request.body.email;
+    //         const password = request.body.password;
+    //         const passwordConfirm = request.body.passwordConfirm;
+
+    //         const errors = [];
+    //         console.log(firstname.length);
+
+    //         // On regarde la taille de la chaine de caractère
+    //         if (firstname.length === 0) {
+    //             // Si elle est à 0, on rajoute une error à notre tableau
+    //             errors.push("Le prénom est obligatoire");
+    //         }
+
+    //         if (lastname.length === 0) {
+    //             errors.push("Le nom de famille est obligatoire");
+    //         }
+
+    //         if (password.length === 0) {
+    //             errors.push("Le mot de passe est obligatoire");
+    //         }
+
+    //         // email valide ?
+    //         if (!email.includes('@')) {
+    //             errors.push("L'email n'est pas valide");
+    //         }
+
+    //         // mots de passes identiques ?
+    //         if (password !== passwordConfirm) {
+    //             errors.push("Les mots de passe sont différents");
+    //         }
+
+    //         // On compte le nombre d'user avec l'email fourni
+    //         const emailDejaPresent = await adminDataMapper.countEmail(email);
+
+
+    //         // Si il est > 0
+    //         if (emailDejaPresent > 0) {
+    //             // On rajoute une erreur
+    //             errors.push("Un compte existe déjà avec cet email");
+    //         }
+
+    //         if (errors.length > 0) {
+    //             // En cas d'erreurs détectées, on fait un rendu de la vue register
+    //             // En lui transmettant notre tableau d'erreur.
+    //             response.render('signup', {
+    //                 error: errors
+    //             });
+    //             return;
+    //         }
+
+    //         const hash = bcrypt.hashSync(password, 10);
+
+    //         // Enregistrer ces données en BDD
+    //         const user = await adminDataMapper.insertOne({
+    //             firstname,
+    //             lastname,
+    //             email,
+    //             password: hash
+    //         });
+
+    //         // Connecter l'utilisateur (l'enregistrer en session)
+    //         request.session.user = user;
+
+    //         // Rediriger l'internaute sur sa page profil
+    //         response.redirect('/');
+
+    //     } catch (error) {
+    //         console.log(error);
+    //         response.json({ error: error.message });
+    //     }
+
+    // },
+
+
 
 
 }
--- back/app/routers/index.js
+++ back/app/routers/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const errorController = require('../controllers/404');
 const userController = require('../controllers/user');
-// const adminController = require('../controllers/admin')
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
@@ -19,9 +19,27 @@ router.get('/logout', userController.logout);
 router.route('/profile/parent/:id')
     .patch(userController.updatePassword)
 
-router.route('/profile/parent/:id/info')
-    .patch(userController.updateUser);
+// router.route('/profile/parent/:id/info')
+//     .patch(userController.updateUser);
 
+
+// adding a user
+router.route('/profile/admin/manageprofile')
+    .post(adminController.addUser);
+
+
+// modifying or deleting a user
+router.route('/profile/admin/manageprofile/:id')
+    // .patch(adminController.modifyUser)
+    .delete(adminController.deleteUser);
+
+
+// router.route('/profile/admin/parent/:id/managechildren')
+//     .post(adminController.addChild);
+
+// router.route('/profile/admin/parent/:id/managechildren/:childid')
+//     //.patch(userController.modifyChild)
+//     .delete(adminController.deleteChild);
 
 router.use(errorController.notFoundResource);
 
