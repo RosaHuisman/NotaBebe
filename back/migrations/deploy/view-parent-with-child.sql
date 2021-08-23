aee06fd948343bbd20a975da6cc9c88583dd06b0
MorganeBENUREAU <morgane.benureau@gmail.com>
2021-08-23 12:15:21 +0200

    sqitch migration for parent_with_child view

---
+++ back/migrations/deploy/view-parent-with-child.sql
@@ -0,0 +1,28 @@
+-- Deploy NotaBebe:view-parent-with-child to pg
+
+BEGIN;
+
+CREATE OR REPLACE VIEW "parent_with_child" AS
+
+SELECT
+"user_has_child"."child_id",
+"user_has_child"."user_id",
+"child"."id" AS "pwc_child_id",
+"child"."first_name" AS "pwc_child_first_name",
+"child"."last_name" AS "pwc_child_last_name",
+"child"."birthdate",
+"child"."birthplace",
+"child"."sex",
+"child"."allergies",
+"user"."id" AS "pwc_user_id",
+"user"."first_name" AS "pwc_user_first_name",
+"user"."last_name" AS "pwc_user_last_name",
+"user"."address",
+"user"."postcode",
+"user"."city"
+
+FROM "user_has_child" 
+	JOIN "child" ON "user_has_child"."child_id" = "child"."id"
+	JOIN "user" ON "user_has_child"."user_id" = "user"."id";
+
+COMMIT;
