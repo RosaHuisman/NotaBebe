4e8db0c331e05341f4f4b88ce8c5efad8185b1fe
RosaHuisman <rosa.huisman@gmail.com>
2021-08-30 15:31:47 +0200

    first step update recap child recap

--- front/src/components/ChildRecap/Day/index.js
+++ front/src/components/ChildRecap/Day/index.js
@@ -13,19 +13,25 @@ const Day = ({
   return (
     <div className="day">  
 
-      <p className="day__designation">Horaires de sieste :</p>
-      <p className="day__result">{recap[0].start_time} - {recap[0].end_time}</p>
+    {recap[0].naps.map((nap) => {
+        return (
+          < div key={nap.id} >
+          <p  className="day__designation">Horaires de sieste :</p>
+          <p className="day__result">{nap.start_time} - {nap.end_time}</p>
+          <p className="day__result">{nap.comment} </p>
+          </div>
+        )
+      })}
 
-       {/* {recap[1] ? (
-         <>
-         <p className="day__designation">Repas :</p>
-         <p className="day__result">{recap[1].rwnam_meal_comment}</p>
-         </>
-       ) : null} */}
+      {recap[0].meals.map((meal) => {
+        return (
+          < div key={meal.id} >
+          <p className="day__designation">Repas :</p>
+          <p className="day__result">{meal.comment}</p>
+          </div>
+        )
+      })} 
 
-      <p className="day__designation">Repas :</p>
-      <p className="day__result">{recap[1].rwnam_meal_comment}</p>
-            
       <p className="day__designation">Infos complémentaires :</p>
       <p className="day__result">{recap[0].extra_info}</p>
 
