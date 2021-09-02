34064c25e09c1c66e8d34a1aa78f57d59ae478a7
Viktk <viktk88@gmail.com>
2021-09-02 17:06:10 +0200

    Fix bug Admin Staff CSS

--- front/src/components/Comments/index.js
+++ front/src/components/Comments/index.js
@@ -3,41 +3,38 @@ import PropTypes from 'prop-types';
 import Loading from 'src/components/App/Loading';
 import { formatDate, formatHour } from 'src/store/selectors/formatDate';
 import { filterComments } from 'src/store/selectors/comment';
+import HeaderStaff from 'src/containers/HeaderStaff';
 
-import { childSearched } from 'src/store/selectors/user'
-
+import { childSearched } from 'src/store/selectors/user';
 
 import './styles.scss';
-//import commentsData from 'src/data/comments';
+// import commentsData from 'src/data/comments';
 
-const Comments = ({ 
+const Comments = ({
 
-  recaps, 
-  onSearchSubmit, 
-  inputRef, 
-  searchValue, 
-  loading, 
-  loadComments, 
+  recaps,
+  onSearchSubmit,
+  inputRef,
+  searchValue,
+  loading,
+  loadComments,
   comments,
   loadChildren,
-  //child,
+  // child,
   children,
   inputValue,
-  onInputValueCommentChange
+  onInputValueCommentChange,
 }) => {
+  console.log(comments);
 
-  console.log(comments)
-
-
-  //console.log(children);
+  // console.log(children);
 
   // console.log('mes données:', commentsData);
 
   useEffect(() => {
     loadChildren();
     loadComments();
-    
-  }, [])
+  }, []);
 
   if (loading) {
     return <Loading />;
@@ -46,41 +43,45 @@ const Comments = ({
   // const [ searchNewValue, setSearchNewValue ] = useState('');
   // const [comments] = useState([]);
 
+  // filterComments(comments, inputValue);
 
-// filterComments(comments, inputValue);
+  return (
+    <>
+      <div>
+        <HeaderStaff />
+      </div>
 
-return (
-  <>
-    <h1>Les commentaires parents</h1>
-    <div className="filter">
-      <form onSubmit={onSearchSubmit}>
-        <input
-          ref={inputRef}
-          className="filter__input"
-          //fluid
-          value={inputValue}
-          onChange={(e) => onInputValueCommentChange(e.target.value)}
-          placeholder="Filtrer les commentaires"
+      <h1>Les commentaires parents</h1>
+      <div className="filter">
+        <form onSubmit={onSearchSubmit}>
+          <input
+            ref={inputRef}
+            className="filter__input"
+          // fluid
+            value={inputValue}
+            onChange={(e) => onInputValueCommentChange(e.target.value)}
+            placeholder="Filtrer les commentaires"
           />
-      </form>
-    </div>
-    <div className="comments__list">
-      { filterComments(comments, inputValue).map((comment) => (
-        <div key={comment.id} className="comment">
+        </form>
+      </div>
+      <div className="comments__list">
+        { filterComments(comments, inputValue).map((comment) => (
+          <div key={comment.id} className="comment">
 
-          <h1 className="comment__child"> {childSearched(children, comment.child_id )}</h1>
-          <p className="comment__child__date">
-          Date: {formatDate(comment.created_at)} à {formatHour(comment.created_at)}
-          </p>
+            <h1 className="comment__child"> {childSearched(children, comment.child_id)}</h1>
+            <p className="comment__child__date">
+              Date: {formatDate(comment.created_at)} à {formatHour(comment.created_at)}
+            </p>
 
-          <p className="comment__message">Message: {comment.message}</p>
-        </div>
-      ))}
-    </div>
+            <p className="comment__message">Message: {comment.message}</p>
+          </div>
+        ))}
+      </div>
 
-    <div>-</div>
+      <div>-</div>
 
-  </>
-)};
+    </>
+  );
+};
 
 export default Comments;
