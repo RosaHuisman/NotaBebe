7c06babaf19db6d2cb4528ed64bda0db9342a43d
blandinejar <blandine.jarysta@gmail.com>
2021-09-03 23:27:57 +0200

    change password length in joi

--- back/app/validations/schemas/user.js
+++ back/app/validations/schemas/user.js
@@ -7,7 +7,7 @@ const userSchema = Joi.object({
     postcode: Joi.string().length(5),
     city: Joi.string().min(2).max(70),
     email: Joi.string().min(3).max(70),
-    password: Joi.string().min(4).max(50),
+    password: Joi.string().min(4).max(100),
     phone_number: Joi.string().length(10),
     role_id: Joi.number().min(1).max(1)
 }).required().min(1).max(9);
