19d8529a7675ef6126f45800d5d4e4ebc5106974
blandinejar <blandine.jarysta@gmail.com>
2021-09-03 09:45:00 +0200

    regex to check ids in router

--- back/app/routers/index.js
+++ back/app/routers/index.js
@@ -45,70 +45,70 @@ router.route('/profile/staff/comments')
 // ROUTES TO DISPLAY SPECIFIC DATA
 
 // get parent by id
-router.route('/profile/parent/:id')
+router.route('/profile/parent/:id(\\d+)')
     .get(userController.getParentById)
     .patch(userController.updateUser);
 
 // modify password (with parent id)
-router.route('/profile/parent/:id/password')
+router.route('/profile/parent/:id(\\d+)/password')
     .patch(userController.updatePassword);
 //? voir pour refacto pour tous les users : /profile/user/:id/password ???
 
 // modify password for STAFF (with user id)
-router.route('/profile/staff/:id/password')
+router.route('/profile/staff/:id(\\d+)/password')
     .patch(userController.updatePassword);
 
 // modify password for ADMIN (with user id)
-router.route('/profile/admin/:id/password')
+router.route('/profile/admin/:id(\\d+)/password')
     .patch(userController.updatePassword);
 
 
 // get child by id - for admin
-router.route('/profile/admin/child/:id')
+router.route('/profile/admin/child/:id(\\d+)')
     .get(adminController.getChildById);
 
 // get child by id - for staff
-router.route('/profile/staff/child/:id')
+router.route('/profile/staff/child/:id(\\d+)')
     .get(adminController.getChildById);
 
 
 
 
 // modify child
-router.route('/profile/parent/:id/child/:childId')
+router.route('/profile/parent/:id(\\d+)/child/:childId(\\d+)')
     .get(userController.getChildFromParent)
     .patch(userController.modifyChild);
 
 // get staff by id
-router.route('/profile/staff/:id')
+router.route('/profile/staff/:id(\\d+)')
     .get(userController.getStaffById);
 // patch -> modifyStaff
 
 // get recap by id
-router.route('/profile/staff/recap/:id')
+router.route('/profile/staff/recap/:id(\\d+)')
     .get(recapController.getRecapById);
 
 // get recaps by child id 
-router.route('/profile/staff/child/:childId/recaps')
+router.route('/profile/staff/child/:childId(\\d+)/recaps')
     .get(recapController.getRecapsByChildId);
 
 
 // get comment by id
-router.route('/profile/staff/comments/:id')
+router.route('/profile/staff/comments/:id(\\d+)')
     .get(commentController.getCommentById);
 
 
 // get comments by child id
-router.route('/profile/staff/comments/child/:childId')
+router.route('/profile/staff/comments/child/:childId(\\d+)')
     .get(commentController.getCommentsByChildId);
 
 
 // get all of one parent's comments - for staff
-router.route('/profile/staff/comments/parent/:parentId')
+router.route('/profile/staff/comments/parent/:parentId(\\d+)')
     .get(commentController.getCommentsByParentId);
 
 // get all of one parent's comments - for the parent
-router.route('/profile/parent/:parentId/comments')
+router.route('/profile/parent/:parentId(\\d+)/comments')
     .get(commentController.getCommentsByParentId);
 
 
@@ -117,16 +117,16 @@ router.route('/profile/admin/manageprofile')
     .post(adminController.addUser);
 
 // modifying or deleting a user
-router.route('/profile/admin/manageprofile/:id')
+router.route('/profile/admin/manageprofile/:id(\\d+)')
     .delete(adminController.deleteUser);
 
 // adding a child (for admin)
-router.route('/profile/admin/parent/:id/managechildren')
+router.route('/profile/admin/parent/:id(\\d+)/managechildren')
     .post(adminController.addChild);
 
 
 // modifying a child - for admin
-router.route('/profile/admin/parent/:id/managechildren/:childId')
+router.route('/profile/admin/parent/:id(\\d+)/managechildren/:childId(\\d+)')
     .patch(userController.modifyChild)
     .delete(adminController.deleteChild);
 
@@ -135,35 +135,35 @@ router.route('/profile/staff/child/recap')
     .post(recapController.addRecap);
 
 // adding a nap to a recap
-router.route('/profile/staff/child/recap/:recapId/nap')
+router.route('/profile/staff/child/recap/:recapId(\\d+)/nap')
     .post(recapController.addNap);
 
 // adding a meal to a recap
-router.route('/profile/staff/child/recap/:recapId/meal')
+router.route('/profile/staff/child/recap/:recapId(\\d+)/meal')
     .post(recapController.addMeal);
 
 // modify and delete a recap
-router.route('/profile/staff/child/recap/:recapId/')
+router.route('/profile/staff/child/recap/:recapId(\\d+)/')
     .patch(recapController.modifyRecap)
     .delete(recapController.deleteRecap);
 
 // modify and delete a nap in a recap
-router.route('/profile/staff/child/recap/:recapId/nap/:napId')
+router.route('/profile/staff/child/recap/:recapId(\\d+)/nap/:napId(\\d+)')
     .patch(recapController.modifyNap)
     .delete(recapController.deleteNap);
 
 // modify and delete a meal in a recap
-router.route('/profile/staff/child/recap/:recapId/meal/:mealId')
+router.route('/profile/staff/child/recap/:recapId(\\d+)/meal/:mealId(\\d+)')
     .patch(recapController.modifyMeal)
     .delete(recapController.deleteMeal);
 
 // adding a comment (for parents)
-router.route('/profile/parent/:id/child/:childId/comments')
+router.route('/profile/parent/:id(\\d+)/child/:childId(\\d+)/comments')
     .post(commentController.addComment);
 
 
 // modifying/deleting a comment (for parents)
-router.route('/profile/parent/:id/child/:childId/comments/:commentId')
+router.route('/profile/parent/:id(\\d+)/child/:childId(\\d+)/comments/:commentId(\\d+)')
     .patch(commentController.modifyComment)
     .delete(commentController.deleteComment);
 
