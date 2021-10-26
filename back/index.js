55dbf50ae7776e33a61055114cf915409206c946
MorganeBENUREAU <morgane.benureau@gmail.com>
2021-10-26 11:56:43 +0200

    adding sanitizer

--- back/index.js
+++ back/index.js
@@ -3,13 +3,25 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const cors = require('cors');
+const sanitizer = require('sanitizer');
 
 const router = require('./app/routers');
 
 const app = express();
 app.use(cors({
     origin: '*'
+    // la bonne pratique : origin: 'https://notabebe.netlify.app/'
 }));
+
+// On assainit avec sanitizer
+app.use((req, res, next) => {
+    if (req.body) {
+        for (const prop in req.body) {
+            req.body[prop] = sanitizer.escape(req.body[prop]);
+        }
+    }
+    next();
+});
 
 const port = process.env.PORT || 3000;
 
