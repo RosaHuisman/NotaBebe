e0e95fbd308cfafc2a8dcd7f0be5e2c672629f2e
MorganeBENUREAU <morgane.benureau@gmail.com>
2021-10-26 14:13:19 +0200

    testing cors

--- back/index.js
+++ back/index.js
@@ -9,14 +9,10 @@ const router = require('./app/routers');
 
 const app = express();
 
-// la bonne pratique : origin: 'https://notabebe.netlify.app/'
-const corsOptions ={
-    origin:'https://notabebe.netlify.app', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
-// app.use(cors());
+app.use(cors({
+    origin: 'https://notabebe.netlify.app'
+    // la bonne pratique : origin: 'https://notabebe.netlify.app/'
+}));
 
 // On assainit avec sanitizer
 app.use((req, res, next) => {
